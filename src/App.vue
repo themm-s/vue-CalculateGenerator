9e81ab2297f9bd1aeac7876f198757c3b9c69f86
themms <maximusnewera@mail.ru>
2024-02-03 19:03:43 +0600

    add transfer props and ready for calculate and results

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div class="config">
-  Конфиг:
-  <div>
-    Кол-во примеров: {{ quantity }}
-    <br/>
-    Максимальное кол-во цифр в примере: {{ maxNumber }}
-    <br/>
-    Разрешить ответы с отрицательными числами: 
-    {{ negativeAnswer ? 'Да' : 'Нет' }}
-    <br/>
-    Режим: {{ mode === 'Adventure' ? 'Продвинутый' : 'Стандартный' }}
+  <div v-if="answerComponent">
+    <div class="config">
+      Конфиг:
+      <div>
+        Кол-во примеров: {{ quantity }}
+        <br />
+        Максимальное кол-во цифр в примере: {{ maxNumber }}
+        <br />
+        Разрешить ответы с отрицательными числами:
+        {{ negativeAnswer ? 'Да' : 'Нет' }}
+        <br />
+        Режим: {{ mode === 'Adventure' ? 'Продвинутый' : 'Стандартный' }}
+      </div>
+    </div>
+    <InputsBlock @quantity="quantity = $event" @numbers="maxNumber = $event" />
+    <CheckBoxBlock @inp="negativeAnswer = $event" />
+    <Ratio @inp="mode = $event" />
+    <button @click="Generate">Сгенерировать</button>
   </div>
-</div>
-<InputsBlock
-  @quantity="quantity = $event"
-  @numbers="maxNumber = $event"
-/>
-<CheckBoxBlock
-  @inp="negativeAnswer = $event"
-/>
-<Ratio
-  @inp="mode = $event"
-/>
-<button @click="Generate">Сгенерировать</button>
+  <div v-else>
+    <TableOfAnswers :answer="answer" :examples="examples"/>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -30,31 +28,43 @@ import { ref } from "vue";
 import CheckBoxBlock from "./components/CheckBoxBlock.vue";
 import InputsBlock from "./components/InputsBlock.vue";
 import Ratio from "./ui/Ratio.vue";
+import TableOfAnswers from "./components/TableOfAnswers.vue";
 
-const quantity = ref(5)
-const maxNumber = ref(1)
-const negativeAnswer = ref(false)
-const mode = ref('Default')
-
-function Action() {
-
-}
+const quantity = ref(5);
+const maxNumber = ref(1);
+const negativeAnswer = ref(false);
+const answerComponent = ref(true);
+const mode = ref('Default');
+const examples: string[] = [];
+const answer: string[] = [];
 
 function Generate() {
-  const arr = []
+  examples.splice(0, examples.length);
   for (let i = 0; i < quantity.value; i++) {
-    const numFirst = Math.floor(1 + Math.random() * 100)
-    const numSecond = Math.floor(1 + Math.random() * 100)
-    const operator = ['+', '-', '/', '*'][Math.floor(Math.random() * 4)]
-    arr.push(`${numFirst} ${operator} ${numSecond} = ${eval(numFirst + operator + numSecond)}`)
-    console.log(arr)
+    const numFirst = Math.floor(1 + Math.random() * 100);
+    const numSecond = Math.floor(1 + Math.random() * 100);
+
+    let operator = ['+', '-'][Math.floor(Math.random() * 2)];
+
+    if (mode.value === 'Adventure') {
+      operator = ['+', '-', '/', '*'][Math.floor(Math.random() * 4)];
+    }
+
+    let result = eval(numFirst + operator + numSecond).toFixed(1);
+
+    if (result < 0 && !negativeAnswer.value) { i--; continue; }
+    if (result.includes('.') && result.split('.')[1] == '0') { result = result.split('.')[0]; }
+    answer.push(`${result}`);
+
+    examples.push(`${numFirst} ${operator} ${numSecond}`);
+    console.log(examples);
   }
+  answerComponent.value = false;
 }
 
 </script>
 
 <style>
-
 .config {
   margin: auto;
   width: 600px;
@@ -64,6 +74,5 @@ function Generate() {
   border-radius: 5px;
   margin-bottom: 20px;
 }
-
 </style>
 
---
+++ src/components/TableOfAnswers.vue
@@ -0,0 +1,26 @@
+<template>
+  {{ test = answer }}
+  <div v-for="example, index in examples">
+    <label>{{ example }} =
+    <input 
+      type="number" 
+      @input="checkResult(index)" 
+      :key="index" 
+    />
+  </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+defineProps<{
+  examples: string[];
+  answer: string[];
+}>()
+
+let test: string[] = []
+setTimeout(() => {
+  console.log(test)
+}, 5000);
+
+</script>
