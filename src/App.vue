66dc48546d1b3aeb53b2e8feb584997681cc0b0b
themms <maximusnewera@mail.ru>
2024-02-03 14:36:03 +0600

    firststage testing generate function

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,54 @@
   <div>
     Кол-во примеров: {{ quantity }}
     <br/>
-    Минимальное кол-во цифр в примере: {{ minNumber }}
+    Максимальное кол-во цифр в примере: {{ maxNumber }}
     <br/>
     Разрешить ответы с отрицательными числами: 
     {{ negativeAnswer ? 'Да' : 'Нет' }}
+    <br/>
+    Режим: {{ mode === 'Adventure' ? 'Продвинутый' : 'Стандартный' }}
   </div>
 </div>
 <InputsBlock
   @quantity="quantity = $event"
-  @numbers="minNumber = $event"
+  @numbers="maxNumber = $event"
 />
 <CheckBoxBlock
   @inp="negativeAnswer = $event"
 />
+<Ratio
+  @inp="mode = $event"
+/>
+<button @click="generate">Сгенерировать</button>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import CheckBoxBlock from "./components/CheckBoxBlock.vue";
 import InputsBlock from "./components/InputsBlock.vue";
+import Ratio from "./ui/Ratio.vue";
 
 const quantity = ref(5)
-const minNumber = ref(1)
+const maxNumber = ref(1)
 const negativeAnswer = ref(false)
+const mode = ref('Default')
+
+function generate() {
+  for (let i = 0; i < quantity.value; i++) {
+    const numFirst = Math.floor(1 + Math.random() * 100)
+    const numSecond = Math.floor(1 + Math.random() * 100)
+    const action = Math.random() * 10
+    if (action < 2.5) {
+      console.log(`${numFirst} + ${numSecond} = ${numFirst + numSecond}`)
+    } else if (action > 2.5 && action < 5) {
+      console.log(`${numFirst} - ${numSecond} = ${numFirst - numSecond}`)
+    } else if (action > 5 && action < 7.5) {
+      console.log(`${numFirst} * ${numSecond} = ${numFirst * numSecond}`)
+    } else {
+      console.log(`${numFirst} / ${numSecond} = ${numFirst / numSecond}`)
+    }
+  }
+}
 
 </script>
 
---
+++ src/ui/Ratio.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block-radio">
+    <label for="Default">
+    <input type="radio" name="radio" value="Default" id="Default" @change="$emit('inp', 'Default')" />
+    Стандартный режим (+ -)
+  </label>
+  <label for="Adventure">
+    <input type="radio" name="radio" value="Adventure" id="Adventure" @change="$emit('inp', 'Adventure')" />
+    Продвинутый режим (+ - * /)
+  </label>
+  </div>
+</template>
+
+<style>
+.block-radio {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
